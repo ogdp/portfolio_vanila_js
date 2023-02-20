792455fd4745c1146981ede48eefe2b218f953c9
MinhDuc <ogdp.vn@gmail.com>
2023-02-20 12:49:45 +0700

    cpn

--- Global/components/components.js
+++ Global/components/components.js
@@ -2,7 +2,7 @@ import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import Home from "../../pages/Home/";
 import Projects from "../../pages/Projects/projects";
-import About from "../../pages/About/About";
+import About from "../../pages/About/about";
 import Contact from "../../pages/Contact/contact";
 import Posts from "../../pages/Posts/Posts";
 import ProjectDetail from "../../pages/Projects/projectDetail";
